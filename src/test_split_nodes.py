83864c7efb38cfe472ac4a1b6a0ee3a223a2503a
WaleedJumaa98 <wwaleed.jumaa@gmail.com>
2026-04-15 18:59:42 +0200

    Split text nodes with tests

---
+++ src/test_split_nodes.py
@@ -0,0 +1,37 @@
+import unittest
+from textnode import TextType, TextNode
+from split_nodes import split_nodes_delimiter
+
+
+class TestSplitNodes(unittest.TestCase):
+    def test_split_bold(self):
+        nodes = [TextNode("This is **bold** text", TextType.TEXT)]
+        new_nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+        expected = [
+            TextNode("This is ", TextType.TEXT),
+            TextNode("bold", TextType.BOLD),
+            TextNode(" text", TextType.TEXT),
+        ]
+        self.assertEqual(new_nodes, expected)
+
+    def test_split_italic(self):
+        nodes = [TextNode("This is *italic* text", TextType.TEXT)]
+        new_nodes = split_nodes_delimiter(nodes, "*", TextType.ITALIC)
+        expected = [
+            TextNode("This is ", TextType.TEXT),
+            TextNode("italic", TextType.ITALIC),
+            TextNode(" text", TextType.TEXT),
+        ]
+        self.assertEqual(new_nodes, expected)
+
+    def test_unmatched_delimiter(self):
+        nodes = [TextNode("This is **bold text", TextType.TEXT)]
+        with self.assertRaises(Exception) as context:
+            split_nodes_delimiter(nodes, "**", TextType.BOLD)
+        self.assertTrue(
+            "Invalid markdown: missing closing delimiter" in str(context.exception)
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
--- src/test_text_node_to_html_node.py
+++ src/test_text_node_to_html_node.py
@@ -6,7 +6,7 @@ from node_html_converter import text_node_to_html_node
 
 class TestTextNodeToHtmlNode(unittest.TestCase):
     def test_text(self):
-        node = TextNode("This is a text node", TextType.PLAIN)
+        node = TextNode("This is a text node", TextType.TEXT)
         html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, None)
         self.assertEqual(html_node.value, "This is a text node")
